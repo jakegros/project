e0dfa102fd06f740c437a8c644a58bd2075e07d9
Jacob Grossman <133597504+jakegros@users.noreply.github.com>
2023-07-25 02:17:45 -0600

    Update primary.js

--- scripts/primary.js
+++ scripts/primary.js
@@ -1,14 +1,29 @@
 <script>
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+
+// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (event.target.matches('.dropbtn')) {
-    var dropbuttons = document.getElementsByClassName("dropbtn");
-    for (var i = 0; i < dropbuttons.length; i++) {
-      var openDropdown = dropbuttons[i];
-      if (openDropdown.classList.contains('show') && !event.target.classList.contains('show')) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
-    event.target.classList.toggle("show");
   }
 }
   </script>
