530ebda676953e4f0b7fa93a5056199ff40976e9
Jacob Grossman <133597504+jakegros@users.noreply.github.com>
2023-07-30 23:27:18 -0600

    Create form.js

---
+++ scripts/form.js
@@ -0,0 +1,21 @@
+<script>
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+
+    // Additional validation if needed before sending the form data?
+
+    // This is a simulated form submission
+    // Replace this with actual form submission to a backend endpoint using Fetch API
+    setTimeout(() => {
+        document.getElementById("status").textContent = "Form submitted successfully!";
+        document.getElementById("contact-form").reset();
+    }, 1000);
+});
+
+</script>
